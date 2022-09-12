861fc6f02bf8d3dcd71c65fc1f5606dc7e5577cd
S Gayathri <gayathridhas@gmail.com>
2022-09-12 06:18:42 +0000

    Final

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello YOUTUBE')
+        self.response.write('Hello YOUTUBE, success')
 
 app = webapp2.WSGIApplication([
     ('/',MainHandler)
